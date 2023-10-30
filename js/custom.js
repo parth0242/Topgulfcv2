fd1c2ec266cd4ec7704aaebbc9cd4a1724f2b472
parth0242 <parth.n@cwstechnology.com>
2023-10-30 12:01:26 +0530

    first inti

---
+++ js/custom.js
@@ -0,0 +1,50 @@
+// sticky header
+$(window).scroll(function () {
+    if ($(this).scrollTop() > 1) {
+        $("header").addClass("sticky_header");
+    } else {
+        $("header").removeClass("sticky_header");
+    }
+});
+
+//slider
+$(document).ready(function () {
+    $(".hero-pannel").owlCarousel({
+        loop: true,
+        margin: 30,
+        nav: true,
+        responsiveClass: true,
+        dots: false,
+        items: 1,
+        autoplay: false,
+        navText: [
+            '<span class="arrow-left"><img src="images/slider-left-arrow.png" alt="arrow" /></span>',
+            '<span class="arrow-right"><img src="images/slider-right-arrow.png" alt="arrow" /></span>',
+        ],
+    });
+});
+
+$(document).ready(function () {
+    var owl = $('.logo-pannel');
+    owl.owlCarousel({
+        items: 5,
+        loop: false,
+        dots: false,
+        nav: false,
+        margin: 20,
+        autoplay: true,
+        slideTransition: 'linear',
+        autoplayTimeout: 0,
+        autoplaySpeed: 30000,
+        responsive: {
+            0: {
+                items: 3,
+            },
+            768: {
+                items: 5,
+            },
+        },
+
+    });
+
+});
